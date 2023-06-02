1872e8ccc28ce9ca7432baed09595c70a5dd33d2
Ali Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-06-02 09:49:27 +0330

    add left membership fuction

--- fuzzy_controller.py
+++ fuzzy_controller.py
@@ -13,4 +13,25 @@ class FuzzyController:
         main method for doin all the phases and returning the final answer for rotation
         """
         return 0
-    
+
+    def membership_degree_left(self, left_dist):
+        return {'close_L': self.close_L(left_dist),
+                'moderate_L': self.moderate_L(left_dist),
+                'far_L': self.far_L(left_dist)}
+
+    def close_L(self, x):
+        if 0 <= x <= 50:
+            return (-1/50) * x + 1
+        return 0
+
+    def moderate_L(self, x):
+        if 35 <= x <= 50:
+            return (1/15) * x - (7/3)
+        if 50 < x <= 65:
+            return (-1/15) * x + (13/3)
+        return 0
+
+    def far_L(self, x):
+        if 50 <= x <= 100:
+            return (1/50) * x - 1
+        return 0
